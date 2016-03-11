1a1c05fb8e66baab7aed8938380cb755f3667ac6
Simon Ochsenreither <simon@ochsenreither.de>
2016-03-11 17:50:45 +0100

    Work on frontpage JS

--- resources/js/frontpage.js
+++ resources/js/frontpage.js
@@ -26,15 +26,14 @@ function resizeSnippetBoxesIn(id) {
   makeAllBoxesSameHeight(snippets);
 }
 
-let requestedSnippet = undefined;
-let expandedExplanation = undefined;
+
+const snippetAndExpansionState = {};
+
 // Figure out how the page is laid out so that we know where to place the div
 // with the expanded explanations. Find the last element which shares the
 // same vertical offset as the one the user clicked on.
-function findExpansionTarget(elem) {
-  if (expandedExplanation !== undefined)
-    expandedExplanation.remove();
-  const snippets = $("#nutshell .snippet-link");
+function findExpansionTarget(container, elem) {
+  const snippets = $("" + container + " .snippet-link");
   const len = snippets.length;
   let i = 0;
   let snippet = undefined;
@@ -43,32 +42,54 @@ function findExpansionTarget(elem) {
     let newSnippet = snippets.get(i);
     if ($(snippet).is(elem)) {
       elemFound = true;
-      console.log("elemFound!")
     }
     if (snippet !== undefined && elemFound && snippet.offsetTop !== newSnippet.offsetTop)
       break;
     snippet = newSnippet;
     i += 1;
   }
-  return snippet;
+  return snippet.nextElementSibling;
 }
 
-function showRequestedSnippet(requested) {
-  let req = undefined;
-  if (requested !== undefined) {
-    if (requested === requestedSnippet) return;
-    requestedSnippet = requested;
+/*
+ * toggle: if true, removes the explanation box if it is active and requested
+ *         if false, does nothing if the explanation box is active and requested
+ */
+function showRequestedSnippet(container, requestedSnippetName, toggle) {
+  console.log(snippetAndExpansionState);
+  const state = snippetAndExpansionState[container];
+  const snippetNamesEqual     = requestedSnippetName === state.activeSnippetName;
+  const snippetNamesDifferent = !snippetNamesEqual;
+  if (state.activeSnippetName !== undefined && (snippetNamesDifferent || snippetNamesEqual && toggle))
+    $(state.activeSnippetName).removeClass("bullet-point-active");    
+  const newExpansionTarget = findExpansionTarget(container, requestedSnippetName)
+  if (newExpansionTarget !== state.activeExpansionTarget || snippetNamesDifferent) {
+    state.activeSnippetName = requestedSnippetName;
+    if (state.activeSnippetExplanation !== undefined) {
+      state.activeSnippetExplanation.hide("fast");
+      state.activeSnippetExplanation.remove();
+      $(newExpansionTarget).removeClass("col-md-12").removeClass("code-snippet");
+    }
+    state.activeExpansionTarget = newExpansionTarget;
+    $(state.activeSnippetName).addClass("bullet-point-active")
+    state.activeSnippetExplanation =
+      $("#hidden-" + state.activeSnippetName.substring(1))
+        .clone(true);
+    $(newExpansionTarget).append(state.activeSnippetExplanation);
+    $(newExpansionTarget).addClass("col-md-12").addClass("code-snippet");
+    state.activeSnippetExplanation.show("fast");
+  } else if (state.activeSnippetName !== undefined && snippetNamesEqual && toggle) {
+    state.activeSnippetExplanation.hide("fast", state.activeSnippetExplanation.remove);
+    $(newExpansionTarget).removeClass("col-md-12").removeClass("code-snippet");
+    state.activeSnippetName = undefined;
+    state.activeSnippetExplanation = undefined;
+    state.activeExpansionTarget = undefined;
   }
-  req = requestedSnippet;
-  if (req === undefined) return;
-  const snip = findExpansionTarget(req);
-  console.log(snip);
-  expandedExplanation =
-    $("#hidden-" + req.substring(1))
-      .clone(true)
-      .addClass("code-snippet");
-  expandedExplanation
-    .insertAfter(snip).slideDown();
+}
+
+function refreshPositionOfExpandedSnippets() {
+  for (let elem in snippetAndExpansionState)
+    showRequestedSnippet(elem.activeSnippetName, false);
 }
 
 $(document).ready(function(){
@@ -87,40 +108,15 @@ $(document).ready(function(){
 
   resizeAllSnippetBoxes();
 
-  // expanding code snippets (front page)
-  function expandSnippetAction(snippetID) {
-    const codeBox = findExpansionTarget(snippetID);
-    console.log(codeBox);
+  ["#nutshell", "#editors", "#tooling"].forEach(
+    cat => snippetAndExpansionState[cat] = { activeSnippetName: undefined, activeSnippetExplanation: undefined, activeExpansionTarget: undefined });
 
-    function go() {
-      const snippet = $(snippetID).html();
+  ["#java-interop", "#expressive", "#concurrency-distribution", "#traits", "#pattern-matching", "#higher-order-functions"]
+    .forEach(it => $(it).click(()=> showRequestedSnippet("#nutshell", it, true)));
 
-      // for positioning the arrow
-      const arrow = $(this).parent().siblings(".code-snippet-arrow");
-      const centerPoint = $(this).position().left + $(this).width()/2;
-      arrow.css("left", centerPoint);
+  ["#eclipse", "#intellij", "#emacs", "#sublime", "#atom", "#vim"]
+    .forEach(it => $(it).click(()=> showRequestedSnippet("#editors", it, true)));
 
-      const codeSnippetInContainer = codeBox.html();
-
-      if (container.is(":hidden")) {
-        arrow.show();
-        arrow.addClass("hover");
-        codeBox.html(snippet);
-        container.slideDown();
-      } else if (codeSnippetInContainer == snippet) {
-        container.slideUp(() => arrow.hide());
-      } else {
-        const hgt = $(snippetID).height();
-        arrow.addClass("hover");
-        codeBox.html(snippet);
-        codeBox.animate({height: hgt}, 400);
-      }
-    }
-    return go;
-  }
-
-  ["#java-interop", "#type-inference", "#concurrency-distribution", "#traits", "#pattern-matching", "#higher-order-functions"]
-    .forEach(it => $(it).click(()=> showRequestedSnippet(it)));
 
   // truncate main visible news item if it exceeds height of sidebar
   const sideboxHgt = $(".recent-news-items").height();
@@ -134,4 +130,4 @@ $(document).ready(function(){
 });
 
 $(window).resize(resizeAllSnippetBoxes);
-$(window).resize(() => showRequestedSnippet());
+$(window).resize(refreshPositionOfExpandedSnippets);
